6c5591c989744bcf0fc13edff9901daeeb5d171f
DevilDogs117 <agmmproductions@gmail.com>
2014-05-21 19:51:35 -0400

    Update GridManager.java

--- GridManager.java
+++ GridManager.java
@@ -1,5 +1,23 @@
 
-public class GridManager
+public class GridManager implements ActionListener
 {
+    
+    private JButton move;
+    private JButton attack;
+    private JButton spells;
+    private JButton inventory;
+    private JButton close;
+    private JButton info;
+    
+    private JFrame optionFrame;
+    
+    public GridManager(){
+        move = new JButton("Move");
+        attack = new JButton("Attack");
+        spells = new JButton("Spells");
+        inventory = new JButton("Inventory");
+        info = new JButton("Info");
+        close = new JButton("Close");
+    }
     
 }
