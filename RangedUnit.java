b2ff3de9044cb18513d0139a583edbc2f9341a40
DevilDogs117 <agmmproductions@gmail.com>
2014-05-23 16:07:38 -0400

    Update RangedUnit.java

--- RangedUnit.java
+++ RangedUnit.java
@@ -1,5 +1,20 @@
+package Units;
+
+import java.awt.Graphics;
+
 
 public class RangedUnit extends Unit
 {
+
+	public RangedUnit(int x, int y){
+		super(x, y);
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
