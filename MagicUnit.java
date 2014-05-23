0384df1603c31e890da389f7bfec3e30dc560fff
DevilDogs117 <agmmproductions@gmail.com>
2014-05-23 16:06:44 -0400

    Update MagicUnit.java

--- MagicUnit.java
+++ MagicUnit.java
@@ -1,7 +1,14 @@
+package Units;
+
 
 public abstract class MagicUnit extends Unit
 {
     private String[] spells = new String[10];
+    
+    public MagicUnit(int x, int y){
+		super(x, y);
+		
+	}
     
     protected String[] getSpells()
     {
