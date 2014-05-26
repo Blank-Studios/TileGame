6037f2fbe0aeb54c574e0caaa76f0a1efe71f8ed
DevilDogs117 <agmmproductions@gmail.com>
2014-05-26 02:31:43 -0400

    Update Healer.java

--- Healer.java
+++ Healer.java
@@ -7,14 +7,14 @@ import Core.GameTile;
 
 public class Healer extends MagicUnit
 {
-    public Healer(GameTile tile, int playerOwner, String name, int level, int[] atts, boolean fly, String[] inventory)
-    {
-        super(tile, playerOwner);
-    }
+	public Healer(GameTile tile, int playerOwner, String name, int level, int[] atts, boolean fly, String[] inventory)
+	{
+		super(tile, playerOwner);
+	}
 
 	@Override
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
